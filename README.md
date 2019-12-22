cc1c0f4f95315e66fc5cf25f1d7f63ce16451030
EgorGo23 <egor.go2303@gmail.com>
2019-12-22 19:46:45 +0300

    create brain-even

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Brain Games
 [![Maintainability](https://api.codeclimate.com/v1/badges/f0368852f99fc4f86e20/maintainability)](https://codeclimate.com/github/EgorGo23/frontend-project-lvl1/maintainability)
 [![Build Status](https://travis-ci.org/EgorGo23/frontend-project-lvl1.svg?branch=master)](https://travis-ci.org/EgorGo23/frontend-project-lvl1)
+https://asciinema.org/a/vWQnp8zhpZBDDrqBnTKE1H0Ye
--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
 import readlineSync from 'readline-sync';
 
 const userName = () => readlineSync.question('May I have your name? ');
-const answer = () => readlineSync.question('Your answer: ');
 
-export { userName, answer };
+const ansFromUser = () => readlineSync.question('Your answer: ');
+
+const randomIntegerNumber = (min, max) => {
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
+
+const parityСheck = (num) => ((num % 2 === 0) ? 'yes' : 'no');
+
+const brainEvenGame = (name) => {
+  let i = 1;
+  while (i <= 3) {
+    const randomNum = randomIntegerNumber(1, 100);
+    const rightAns = parityСheck(randomNum);
+
+    console.log(`Question: ${randomNum}`);
+
+    const ans = ansFromUser();
+
+    if (rightAns !== ans.toLowerCase()) {
+      console.log(`${ans} is wrong answer ;(. Correct answer was ${rightAns}.
+            Let's try again, ${name}!`);
+      break;
+    } else {
+      console.log('Correct!');
+      i += 1;
+    }
+  }
+  if (i === 4) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
+
+export { userName, brainEvenGame };
