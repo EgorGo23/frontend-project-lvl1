125591437155cd2cb2dc504de2c0b2a4b04c59dd
EgorGo23 <egor.go2303@gmail.com>
2020-01-04 23:07:51 +0300

    fix progression game

--- src/brainProgression.js
+++ src/brainProgression.js
@@ -1,8 +1,21 @@
 import randomIntegerNumber from './randomIntegerNumber';
 
+const progressionGeneration = (numberOfElements) => {
+  const firstElement = randomIntegerNumber(1, 500);
+  const step = randomIntegerNumber(0, numberOfElements);
+
+  // eslint-disable-next-line prefer-const
+  let progression = [firstElement];
+  for (let i = 1; i < numberOfElements; i += 1) {
+    progression.push(firstElement + i * step);
+  }
+
+  return progression;
+};
+
 const brainProgression = () => {
-  const progression = [5, 7, 9, 11, 13, 15, 17, 19, 21, 23];
-  const randomNumber = randomIntegerNumber(0, 9);
+  const progression = progressionGeneration(10);
+  const randomNumber = randomIntegerNumber(0, progression.length - 1);
 
   const rightAnswer = progression.splice(randomNumber, 1, '..').join();
 
